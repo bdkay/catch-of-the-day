d9a4cc50a7fe84d4ea1176bf8e7e1cca47679036
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-13 16:38:01 -0500

    Props to props

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){
+    return (
+      <header className="top">
+        <h1>Catch of the Day</h1>
+        <div className="menu">
+          <h3 className="tagline">{ this.props.tagline }</h3>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
