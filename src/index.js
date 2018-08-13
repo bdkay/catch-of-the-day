2657af6aa1716d6b84072305644bccd458a68861
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-13 17:40:35 -0500

    Router v4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './css/style.css';
 
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        { /* When at the homepage, show the StorePicker component */ }
+        <Route exactly pattern="/" component={StorePicker} />
+        { /* After a storeId has been set, show the main app component */ }
+        <Route exactly pattern="/store/:storeId" component={App} />
+        <Route component={StorePicker} />
+      </Switch>
+    </Router>
+  );
+};
 
 render(<App />, document.getElementById('main'));
