f480b88e618b78266909cb1c664b000887e37a6f
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-13 18:06:04 -0500

    Regex

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
   render(){
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          defaultValue={ getFunName() }
+          required placeholder="Store Name"
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
