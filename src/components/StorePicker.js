0b34698b54b84e80729912cdbcf1569075c88af0
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-13 18:48:06 -0500

    Events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  goToStore(event){
+    event.preventDefault();
+    console.log("You changed the url");
+    // First grab the text from the box
+    
+    // Transition from / to /store/:storeId
+  }
+  
   render(){
+    // We can use this inside render because render is always bound to the component
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
         <input
           type="text"
+          ref={(input) => { this.storeInput = input; }}
           defaultValue={ getFunName() }
           required placeholder="Store Name"
         />
