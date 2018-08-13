0b7664d7da9b10a9db1fa643d10f2c262435e523
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-13 17:13:53 -0500

    Styled components

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,13 @@ import Inventory from './Inventory';
 class App extends Component {
   render(){
     return (
-      <div className='catch-of-the-day'>
-        <div className='menu'>
-          <Header tagline="Fresh Seafood Daily" />
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header
+            age="5000"
+            tagline="Brendon is cool"
+            cool={ true }
+          />
         </div>
         <Order />
         <Inventory />
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ class Header extends Component {
   render(){
     return (
       <header className="top">
-        <h1>Catch of the Day</h1>
+        <h1>Catch
+          <span className="ofThe">
+            of the
+          </span>
+          Day
+        </h1>
         <div className="menu">
           <h3 className="tagline">{ this.props.tagline }</h3>
         </div>
