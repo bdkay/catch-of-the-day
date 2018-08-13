d39077f2d37c607aad03d0d8f93f397adfe13643
Brendon Kuleck <brendon@fastmodeltechnologies.com>
2018-08-13 14:53:18 -0500

    CSS

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 
 class StorePicker extends Component {
   render(){
     return (
-      <p>Hello</p>
+      <form className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
     );
   }
 }
